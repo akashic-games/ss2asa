54f2a9683090c4b883c7fa5657f8b98f16c2b684
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2021-02-15 20:07:00 +0900

    Remove Unnecessary fix

--- spec/UtilsSpec.js
+++ spec/UtilsSpec.js
@@ -1,20 +1,10 @@
-var mockfs = require("mock-fs");
 var Utils = require("../lib/Utils.js");
 
 describe("Utils.", function () {
 
-	afterEach(function () {
-		mockfs.restore();
-	});
-
 	describe("loadXmlAsJsAsync", function() {
 		it("can load an xml file and then return it in json", function(done) {
-			mockfs({
-				"src.xml": "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?><SpriteStudioProject version=\"1.02.00\"><name>pl00_all_motion</name></SpriteStudioProject>"
-			});
-
-			Utils.loadXmlAsJsAsync("src.xml", function(err, result) {
-				console.log("@@err", err);
+			Utils.loadXmlAsJsAsync("./spec/support/src.xml", function(err, result) {
 				expect(err).toBeFalsy();
 				expect(result.SpriteStudioProject.name[0]).toBe("pl00_all_motion");
 				done(); // 非同期処理はこのようにして終了を伝える
--- src/Utils.ts
+++ src/Utils.ts
@@ -2,17 +2,13 @@ import fs = require("fs");
 import xml2js = require("xml2js");
 
 export function loadXmlAsJsAsync(fname: string, callback: (err: any, result: any) => void): void {
-	console.log("*** loadXmlAsJsAsync start", fname);
 	fs.readFile(fname, {encoding: "utf8"}, (err: Error, data: string) => {
-		console.log("*** loadXmlAsJsAsync readFile");
 		if (! err) {
-			console.log("*** loadXmlAsJsAsync done");
 			const parser = new xml2js.Parser();
 			parser.parseString(data, (err: any, result: any) => {
 				callback(err, result);
 			});
 		} else {
-			console.log("*** loadXmlAsJsAsync err");
 			callback(err, data);
 		}
 	});
