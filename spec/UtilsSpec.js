ea24c33f6308540b6f808536894079239fbcb16e
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2021-02-15 19:51:52 +0900

    debug ci

--- spec/UtilsSpec.js
+++ spec/UtilsSpec.js
@@ -1,5 +1,9 @@
-var mockfs = require("mock-fs");
+// var mockfs = require("mock-fs");
 var Utils = require("../lib/Utils.js");
+
+// var fs = require("fs");
+// // auto-mock fs
+// jest.mock("fs");
 
 describe("Utils.", function () {
 
@@ -9,11 +13,19 @@ describe("Utils.", function () {
 
 	describe("loadXmlAsJsAsync", function() {
 		it("can load an xml file and then return it in json", function(done) {
-			mockfs({
-				"src.xml": "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?><SpriteStudioProject version=\"1.02.00\"><name>pl00_all_motion</name></SpriteStudioProject>"
-			});
+			// mockfs({
+			// 	"src.xml": "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?><SpriteStudioProject version=\"1.02.00\"><name>pl00_all_motion</name></SpriteStudioProject>"
+			// });
+			// jest.mock("fs", () => {
+			// 	return {
+			// 		readFile: jest.fn((path, opt, cb) => {
+			// 			cb(null, "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?><SpriteStudioProject version=\"1.02.00\"><name>pl00_all_motion</name></SpriteStudioProject>");
+			// 		})
+			// 	};
+			// });
 
-			Utils.loadXmlAsJsAsync("src.xml", function(err, result) {
+			Utils.loadXmlAsJsAsync("./spec/support/src.xml", function(err, result) {
+				console.log("**err", err);
 				expect(err).toBeFalsy();
 				expect(result.SpriteStudioProject.name[0]).toBe("pl00_all_motion");
 				done(); // 非同期処理はこのようにして終了を伝える
--- src/Utils.ts
+++ src/Utils.ts
@@ -2,17 +2,17 @@ import fs = require("fs");
 import xml2js = require("xml2js");
 
 export function loadXmlAsJsAsync(fname: string, callback: (err: any, result: any) => void): void {
-	console.log("*** loadXmlAsJsAsync start");
+	console.log("*** loadXmlAsJsAsync start", fname);
 	fs.readFile(fname, {encoding: "utf8"}, (err: Error, data: string) => {
-		console.log("*** loadXmlAsJsAsync", fname);
+		console.log("*** loadXmlAsJsAsync readFile");
 		if (! err) {
-			console.log("*** loadXmlAsJsAsync err", err);
+			console.log("*** loadXmlAsJsAsync done");
 			const parser = new xml2js.Parser();
 			parser.parseString(data, (err: any, result: any) => {
 				callback(err, result);
 			});
 		} else {
-			console.log("*** loadXmlAsJsAsync done");
+			console.log("*** loadXmlAsJsAsync err");
 			callback(err, data);
 		}
 	});
