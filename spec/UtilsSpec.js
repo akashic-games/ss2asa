e258c6e0403b70615ad6b79301530cee28640755
BlackSpotBear <blackspotbear@users.noreply.github.com>
2024-09-18 14:48:52 +0900

    Return promise to jest

--- spec/UtilsSpec.js
+++ spec/UtilsSpec.js
@@ -2,11 +2,11 @@ var Utils = require("../lib/Utils.js");
 
 describe("Utils.", () => {
 	describe("loadXmlAsJsAsync", () => {
-		it("can load an xml file and then return an object", () =>
-			Utils.loadXMLFileAsyncPromise("./spec/support/src.xml")
+		it("can load an xml file and then return an object", () => {
+			return Utils.loadXMLFileAsyncPromise("./spec/support/src.xml")
 				.then(result => {
 					expect(result.SpriteStudioProject.name[0]).toBe("pl00_all_motion");
 				})
-		);
+		});
 	});
 });
